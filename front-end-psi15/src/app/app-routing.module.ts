f64904b59dc0fc433374378c74ebf63caa2b887e
GoncasCC <goncalo.cor@gmail.com>
2023-04-30 08:32:20 +0100

    Finished login interface
    Finished login interface

--- front-end-psi15/src/app/app-routing.module.ts
+++ front-end-psi15/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main-page', pathMatch: 'full'},
   { path: 'main-page', component: MainPageComponent},
-  { path: 'user-register', component: RegisterPageComponent}
+  { path: 'user-register', component: RegisterPageComponent},
+  { path: 'login', component: LoginPageComponent}
 
 ];
 
--- front-end-psi15/src/app/login-page/login-page.component.ts
+++ front-end-psi15/src/app/login-page/login-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login-page',
+  selector: 'login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+username: any;
+password: any;
+login() {
+throw new Error('Method not implemented.');
+}
 
 }
