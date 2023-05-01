202bf463b555ddd6eb35addf7e7603e95cd7d271
fc56321 <utilizador@exemplo.com>
2023-05-01 23:43:56 +0100

    Dashboard

--- front-end-psi15/src/app/app-routing.module.ts
+++ front-end-psi15/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main-page', pathMatch: 'full'},
   { path: 'main-page', component: MainPageComponent},
   { path: 'user-register', component: RegisterPageComponent},
-  { path: 'user-login', component: LoginPageComponent}
+  { path: 'user-login', component: LoginPageComponent},
+  { path: 'dashboard', component: DashboardComponent}
 
 ];
 
