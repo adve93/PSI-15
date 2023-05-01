c796873507625f8a73eff42c0f291d44b0fe3753
fc56299 <fc56299@alunos.fc.ul.pt>
2023-05-01 23:16:16 +0100

    US6- Começo do checkout
    Para a maior parte das cenas que faltam preciso que vcs ja tenham os objetos a serem mostrados, etc

--- front-end-psi15/src/app/checkout/checkout.component.ts
+++ front-end-psi15/src/app/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { UserService } from '../user.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
@@ -9,13 +9,23 @@ export class CheckoutComponent {
  // items = this.cartService.getItems();
  // total = this.cartService.getTotal();
 
-  constructor(/*private cartService: CartService*/) { }
-
+// constructor(/*private cartService: CartService*/) { }
+ constructor(private userService:UserService) { }
+ 
   ngOnInit(): void {
   }
 
-  checkout(): void {
-    // TODO: Implement checkout logic here
+  checkout(username:string, wallet:number): void {
     console.log('Checkout button clicked');
+    username = username.trim();
+    var hasMoney = this.userService.hasMoney(username,wallet); 
+    if(hasMoney){
+      console.log('tem grana, pode continuar!');
+      // TODO: Implement checkout logic here
+    }else{
+      console.log('Sem grana!');
+    }
+    
   }
+  
 }
