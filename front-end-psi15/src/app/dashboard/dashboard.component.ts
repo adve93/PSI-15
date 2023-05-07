956f54334d4f622b68a2cb24f1f5ab12e2ff0776
Francisco Texeira <fc56305@alunos.fc.ul.pt>
2023-05-07 18:21:39 +0100

    Item search implemented

--- front-end-psi15/src/app/dashboard/dashboard.component.ts
+++ front-end-psi15/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Item } from '../item';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,14 +11,21 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent{
 
+  items: Item[] = [];
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private itemService: ItemService){}
 
   showNotImplemented(){
     window.alert('Feature not implemented.');
   }
 
-  goToUserProfile(){
+
+  getItems(): void{
+    this.itemService.getItemList().subscribe(items => this.items = items.slice(1, 5));
+  }
+
+
+  goToUserProfile(): void{
     this.router.navigate(['/user-details']);
   }
 }
