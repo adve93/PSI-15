fa43b41d6333f3f8ac590a661fffe43817f1118d
fc56299 <fc56299@alunos.fc.ul.pt>
2023-05-13 23:30:19 +0100

    PPG1-60 falta testar, mas acho que ta tudo
    Se eu n me engano ca ta td

--- front-end-psi15/src/app/user-update/user-update.component.ts
+++ front-end-psi15/src/app/user-update/user-update.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { User } from '../user';
 import { UserService } from '../user.service';
-
+import { User } from '../user';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-update',
   templateUrl: './user-update.component.html',
@@ -9,9 +9,9 @@ import { UserService } from '../user.service';
 })
 export class UserUpdateComponent {
   
-  constructor( private userService: UserService) { }
+  constructor( private userService: UserService,private router: Router) { }
   returnToProfile(){
-
+    this.router.navigate(['/user-profile']);
   }
   updateUser(username: String, imagePath:String){
     this.userService.postUpdateUser(username, imagePath);
